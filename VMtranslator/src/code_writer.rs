8f78c5a4ccb42712ae48cc9168c7c5569c1dc758
kjakx <k.jak.xg0g11@gmail.com>
2022-05-24 08:01:30 +0000

    implementing write_arithmetic (in progress)

--- VMtranslator/src/code_writer.rs
+++ VMtranslator/src/code_writer.rs
@@ -1,5 +1,4 @@
-use std::io::BufWriter;
-use std::io::Write;
+use std::io::{BufWriter, Write};
 use std::fs::File;
 use std::path::Path;
 
@@ -26,6 +25,72 @@ impl CodeWriter {
 
     pub fn write_arithmetic(&self, command: String) {
         // translate arithmetic command to .asm
+        match command {
+            "add" => {
+                writeln!(self.writer, "@SP").unwrap();    // a = 1
+                writeln!(self.writer, "AM=M-1").unwrap(); // m[1] = m[1] - 1, a = m[1](means a = SP - 1)
+                writeln!(self.writer, "D=M").unwrap();    // d = m[SP - 1]
+                writeln!(self.writer, "@SP").unwrap();    // a = 1
+                writeln!(self.writer, "AM=M-1").unwrap(); // m[1] = m[1] - 1, a = m[1]
+                writeln!(self.writer, "M=D+M").unwrap();  // m[SP - 2] = d + m[SP - 2]
+                writeln!(self.writer, "@SP").unwrap();    // a = 1
+                writeln!(self.writer, "M=M+1").unwrap();  // m[1] = m[1] + 1
+            },
+            "sub" => {
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "AM=M-1").unwrap();
+                writeln!(self.writer, "D=M").unwrap();
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "AM=M-1").unwrap();
+                writeln!(self.writer, "M=M-D").unwrap(); // x - y
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "M=M+1").unwrap();
+            },
+            "neg" => {
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "AM=M-1").unwrap();
+                writeln!(self.writer, "D=!M").unwrap(); // two's complement
+                writeln!(self.writer, "M=D+1").unwrap();
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "M=M+1").unwrap();
+            },
+            "eq" => {
+                unimplemented!();
+            },
+            "gt" => {
+                unimplemented!();
+            },
+            "lt" => {
+                unimplemented!();
+            },
+            "and" => {
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "AM=M-1").unwrap();
+                writeln!(self.writer, "D=M").unwrap();
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "AM=M-1").unwrap();
+                writeln!(self.writer, "M=M&D").unwrap(); // x & y
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "M=M+1").unwrap();
+            },
+            "or" => {
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "AM=M-1").unwrap();
+                writeln!(self.writer, "D=M").unwrap();
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "AM=M-1").unwrap();
+                writeln!(self.writer, "M=M&D").unwrap(); // x | y
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "M=M+1").unwrap();
+            },
+            "not" => {
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "AM=M-1").unwrap();
+                writeln!(self.writer, "M=!M").unwrap(); // !x
+                writeln!(self.writer, "@SP").unwrap();
+                writeln!(self.writer, "M=M+1").unwrap();
+            },
+        }
     }
 
     pub fn write_push_pop(&self, command: String, segment: String, index: i16) {
